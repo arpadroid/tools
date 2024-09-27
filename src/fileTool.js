00dab968a3140f1600efdde349fb317116310ab5
Andres Vaquero <web.andresvaquero@gmail.com>
2024-09-28 01:16:40 +1000

    adding getFileType and getFileIcon methods

--- src/fileTool.js
+++ src/fileTool.js
@@ -124,3 +124,88 @@ export function processFile(file) {
         file: file instanceof File ? file : null
     };
 }
+
+/**
+ * Returns the type of a file based on its extension.
+ * @param {string | File} ext - If a string, it should be the extension of the file e.g 'jpg', 'pdf'.
+ * If a File object, it will be processed to get the extension.
+ * @returns {string}
+ */
+// eslint-disable-next-line sonarjs/cognitive-complexity
+export function getFileType(ext) {
+    ext instanceof File && (ext = getExtension(ext));
+    if (['jpg', 'jpeg', 'png', 'gif', 'svg', 'bmp', 'tiff', 'webp'].includes(ext)) {
+        return 'image';
+    }
+    if (['mp4', 'mkv', 'avi', 'mov', 'wmv', 'flv', 'webm', 'm4v'].includes(ext)) {
+        return 'video';
+    }
+    if (['mp3', 'wav', 'flac', 'aac', 'ogg', 'm4a', 'wma'].includes(ext)) {
+        return 'audio';
+    }
+    if (['pdf'].includes(ext)) {
+        return 'pdf';
+    }
+    if (['doc', 'docx', 'odt'].includes(ext)) {
+        return 'word';
+    }
+    if (['xls', 'xlsx', 'ods', 'csv'].includes(ext)) {
+        return 'excel';
+    }
+    if (['ppt', 'pptx', 'odp'].includes(ext)) {
+        return 'powerpoint';
+    }
+    if (['zip', 'rar', '7z', 'tar', 'gz', 'bz2', 'xz'].includes(ext)) {
+        return 'compressed';
+    }
+    if (['txt', 'md', 'rtf'].includes(ext)) {
+        return 'text';
+    }
+    if (['html', 'htm', 'xml', 'xhtml'].includes(ext)) {
+        return 'markup';
+    }
+    if (['js', 'ts', 'py', 'java', 'c', 'cpp', 'cs', 'php', 'rb', 'go', 'swift'].includes(ext)) {
+        return 'programming';
+    }
+    if (['exe', 'msi', 'bat', 'sh', 'dmg', 'app', 'apk', 'jar'].includes(ext)) {
+        return 'executable';
+    }
+    if (['iso', 'img', 'bin', 'cue', 'dmg'].includes(ext)) {
+        return 'disk image';
+    }
+    if (['epub', 'mobi', 'azw', 'azw3', 'fb2'].includes(ext)) {
+        return 'ebook';
+    }
+    if (['json', 'yaml', 'yml', 'ini', 'env', 'toml'].includes(ext)) {
+        return 'config';
+    }
+    return 'file';
+}
+
+/**
+ * Returns the icon for a file based on its extension.
+ * @param {string | File} ext - If a string, it should be the extension of the file e.g 'jpg', 'pdf'.
+ * If a File object, it will be processed to get the extension.
+ * @returns {string}
+ */
+export function getFileIcon(ext) {
+    const fileType = getFileType(ext);
+    return {
+        image: 'image',
+        file: 'insert_drive_file',
+        video: 'videocam',
+        audio: 'audiotrack',
+        pdf: 'picture_as_pdf',
+        word: 'description',
+        excel: 'table_chart',
+        powerpoint: 'slideshow',
+        compressed: 'archive',
+        text: 'menu_book',
+        markup: 'code',
+        programming: 'code',
+        executable: 'settings_applications',
+        'disk image': 'disc_full',
+        ebook: 'book',
+        config: 'settings'
+    }[fileType];
+}
