eaf81372aed9a3d975d2ec1bb81b6defc72b2e94
Andres Vaquero <web.andresvaquero@gmail.com>
2024-12-10 22:40:24 +1100

    adding array of objects utils

--- src/arrayTool.js
+++ src/arrayTool.js
@@ -34,3 +34,47 @@ export function arrayToNumbers(array) {
 export function areArraysEqual(array1, array2) {
     return array1.length === array2.length && array1.every((value, index) => value === array2[index]);
 }
+
+/**
+ * Sorts an array of objects by a specified key.
+ * @param {Array} array - The input array.
+ * @param {string} key - The key to sort by.
+ * @param {string} direction - The sort direction.
+ * @returns {Array} - The sorted array.
+ */
+export function sortObjectArrayByKey(array, key, direction = 'asc') {
+    if (!key) return array;
+    return array.sort((itemA, itemB) => {
+        const aVal = itemA[key];
+        const bVal = itemB[key];
+        if (aVal < bVal) return direction === 'asc' ? -1 : 1;
+        if (aVal > bVal) return direction === 'asc' ? 1 : -1;
+        return 0;
+    });
+}
+
+/**
+ * Searches an array of objects for a query within specified fields and returns the results.
+ * @param {Array} array - The input array.
+ * @param {string} query - The search query.
+ * @param {Array} searchFields - The fields to search.
+ * @returns {Array} - The filtered array.
+ */
+export function searchObjectArray(array = [], query, searchFields = ['name']) {
+    if (!query) return [...array];
+    return array.filter(item =>
+        searchFields.some(field => item[field].toLowerCase().includes(query.toLowerCase()))
+    );
+}
+
+/**
+ * Paginates an array.
+ * @param {Array} array - The input array.
+ * @param {number} perPage - The number of items per page.
+ * @param {number} page - The current page.
+ * @returns {Array} - The paginated array.
+ */
+export function paginateArray(array = [], perPage = 10, page = 1) {
+    const start = (page - 1) * perPage;
+    return array.slice(start, start + perPage);
+}
