a69f8bd453d315c547852e3b1f765525e7b4d530
Andres Vaquero <web.andresvaquero@gmail.com>
2025-01-18 09:42:54 +1100

    updating build type

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,3 +1,3 @@
 import { getBuild } from '@arpadroid/arpadroid/src/rollup/builds/rollup-builds.mjs';
-const { build } = getBuild('tools', 'uiComponent');
+const { build } = getBuild('tools', 'library');
 export default build;
