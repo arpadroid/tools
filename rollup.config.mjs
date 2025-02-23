ad6646ee0c59e76a71e278c5d88d2506acc0354d
Andres Vaquero <web.andresvaquero@gmail.com>
2025-02-24 08:34:35 +1100

    migrating repos: arpadroid -> module

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,3 +1,3 @@
-import { getBuild } from '@arpadroid/arpadroid/src/rollup/builds/rollup-builds.mjs';
+import { getBuild } from '@arpadroid/module/src/rollup/builds/rollup-builds.mjs';
 const { build } = getBuild('tools', 'library');
 export default build;
