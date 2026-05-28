566e8ab27295cf287c035f91fc94eeede4ee7629
Andres Vaquero <web.andresvaquero@gmail.com>
2026-05-29 00:52:38 +1000

    moving util to iso

--- src/fileTool/fileTool.js
+++ src/fileTool/fileTool.js
@@ -1,6 +1,11 @@
 /**
  * @typedef {import('./fileTool.types').FilePayloadType} FilePayloadType
  */
+
+import { formatBytes } from '@arpadroid/tools-iso';
+
+export { formatBytes } from '@arpadroid/tools-iso';
+
 /**
  * Get the extension of a file.
  * @param {FilePayloadType} file
@@ -72,29 +77,6 @@ export function getBase64FromUrl(url) {
  */
 export function megaBytesToBytes(megaBytes) {
     return megaBytes * 1024 * 1024;
-}
-
-/**
- * Format bytes to a human-readable string.
- * @param {number} bytes
- * @param {number} [precision]
- * @returns {string}
- */
-export function formatBytes(bytes, precision = 1) {
-    if (bytes === 0) {
-        return '0 bytes';
-    }
-    if (typeof bytes === 'undefined' || !isFinite(bytes)) {
-        return '';
-    }
-    const units = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB'];
-    const $number = Math.floor(Math.log(bytes) / Math.log(1024));
-    const unit = units[$number] ?? 'bytes';
-    if (['KB', 'bytes'].includes(unit)) {
-        precision = 0;
-    }
-    const value = (bytes / Math.pow(1024, Math.floor($number))).toFixed(precision);
-    return (value.match(/\.0*$/) ? value.substr(0, value.indexOf('.')) : value) + ' ' + unit;
 }
 
 /**
